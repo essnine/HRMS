4441ca6672717906e19faf79e8cfd4c5bb11cc6d
szala12 <surajzala12@gmail.com>
2018-03-20 12:14:50 +0530

    Add files via upload

--- bootform3b2.php
+++ bootform3b2.php
@@ -4,18 +4,25 @@ include('include/Connection.php');?>
   <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>HRMS beta</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="res/css/main.css">
+
 </head>
 <body>
+  
+<div class="container head-container">
+                    
+<img src="res/img/head.gif" class="img-responsive">
+  
+</div>
 
 <div class="container">
   <h4 style="font-family: verdana ">STTP/Seminar/Workshop/Symposium/FDP Attended</h4>
-  <p><font face="Verdana" size="1.5px">*All fields are mandetory</font></p>
+  <p><font face="Verdana" size="1.5px">*All fields are mandatory</font></p>
   <form class="form-horizontal">
 
 
--- bootform3b5.php
+++ bootform3b5.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>HRMS beta</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="res/css/main.css">
+
 </head>
 <body>
 
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
+<div class="container head-container">
+                    
+<img src="res/img/head.gif" class="img-responsive">
+  
+</div>
 
 <div class="container">
   <h4 style="font-family: verdana ">Consultancy</h4>
-  <p><font face="Verdana" size="1.5px">*All fields are mandetory</font></p>
+  <p><font face="Verdana" size="1.5px">*All fields are mandatory</font></p>
   <form class="form-horizontal" >
 
 
